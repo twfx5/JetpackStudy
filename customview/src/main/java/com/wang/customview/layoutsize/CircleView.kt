15cd291edfda99f3daa852cb26a6b8d6ccd3652a
wangzh <wangzh526@126.com>
2021-07-23 07:25:41 +0800

    add comment for resolveSize的作用

--- customview/src/main/java/com/wang/customview/layoutsize/CircleView.kt
+++ customview/src/main/java/com/wang/customview/layoutsize/CircleView.kt
@@ -19,7 +19,15 @@ class CircleView(context: Context, attrs: AttributeSet?) : View(context, attrs) 
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         // 自己设置宽高为 400dp x 400dp
         val size = ((PADDING + RADIUS) * 2).toInt()
-        // 根据 MeasureSpec 修正自己的设置
+        /**
+         * resolveSize的作用
+         * 根据 MeasureSpec（MeasureSpec ：父控件对我的限制和LayoutParams）修正自己的大小
+         * LayoutParams 是 xml 中定义的大小 200dp x 200dp
+         * 这样通过 resolveSize 计算后的结果，就和开发中在 xml 中定义的是一样的了
+         *
+         * 如果设置的 200dp x 200dp ，就显示不完整
+         * 如果设置的 wrap_content x wrap_content ，就显示我们自己设置的size 400dp x 400dp
+         */
         val width = resolveSize(size, widthMeasureSpec)
         val height = resolveSize(size, heightMeasureSpec)
         // 将实际的宽高保存下来
