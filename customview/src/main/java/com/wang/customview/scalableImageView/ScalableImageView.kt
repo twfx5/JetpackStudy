9d03935477c3cb3de915da8b9301ef453604edee
wangzh <wangzh526@126.com>
2021-07-28 07:29:54 +0800

    双击缩放 的 ImageView 增加图片的 滑动 和 Fling 松手后滑动

--- customview/src/main/java/com/wang/customview/scalableImageView/ScalableImageView.kt
+++ customview/src/main/java/com/wang/customview/scalableImageView/ScalableImageView.kt
@@ -9,9 +9,12 @@ import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import android.widget.OverScroller
 import androidx.core.view.GestureDetectorCompat
 import com.wang.customview.dp
 import com.wang.customview.getAvatar
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * Created at 2021/7/26 下午10:04.
@@ -19,11 +22,21 @@ import com.wang.customview.getAvatar
  * @author wang
  */
 private val IMAGE_SIZE = 300.dp
+// 放大系数（最大情况下，让 view 可以滑动）
+private const val SCALE_FACTOR = 1.5f
 class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context, attrs),
-    GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+    GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener, Runnable {
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val bitmap = getAvatar(resources, IMAGE_SIZE.toInt())
+
+    // 图片是否是缩小状态
+    private var isSmall = true
+
+    // 初始 x，y 方向的偏移
+    private var originOffsetX = 0f
+    private var originOffsetY = 0f
+
     // x，y 方向的偏移
     private var offsetX = 0f
     private var offsetY = 0f
@@ -34,7 +47,7 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
     }
 
     /**
-     * 将图片放大成，小图，大图时，缩放比例
+     * 将图片分别放大成：小图，大图时，缩放比例
      *
      * 因为手机的高度 > 手机的宽度
      * 小图 ：图片的宽度 放大到 手机的宽度
@@ -46,6 +59,8 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
     /**
      *  属性动画 固定套路 1
      *  属性动画的属性 ：缩放的百分比
+     *
+     *  setValue 后，invalidate()
      */
     private var scaleFraction = 0f
         set(value) {
@@ -56,36 +71,54 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
     /**
      *  属性动画 固定套路 2
      *  延迟初始化：属性动画
+     *
+     *  by lazy {...}
      */
     private val scaleAnimator by lazy {
         ObjectAnimator.ofFloat(this, "scaleFraction", 0f, 1f)
     }
 
-    // 图片是否是缩小状态
-    private var isSmall = true
+    /**
+     *  创建 OverScroller 对象
+     *  OverScroller 就像一个秒表
+     *  我们调用 OverScroller.computeScrollOffset() 就相当于掐一下秒表
+     *  可以得到当前的位置
+     */
+    private val scroller = OverScroller(context)
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         // 让图片居中显示
-        offsetX = (width - IMAGE_SIZE) / 2f
-        offsetY = (height - IMAGE_SIZE) / 2f
+        originOffsetX = (width - IMAGE_SIZE) / 2f
+        originOffsetY = (height - IMAGE_SIZE) / 2f
         if (bitmap.width / bitmap.height.toFloat() > width / height.toFloat()) {
             smallScale = width / bitmap.width.toFloat()
-            bigScale = height / bitmap.height.toFloat()
+            bigScale = height / bitmap.height.toFloat() * SCALE_FACTOR
         } else {
             smallScale = height / bitmap.height.toFloat()
-            bigScale = width / bitmap.width.toFloat()
+            bigScale = width / bitmap.width.toFloat() * SCALE_FACTOR
         }
     }
 
+    /**
+     *  canvas 多次变化，所以倒着看代码
+     *  1、中心位置画图片
+     *  2、缩放
+     *  3、移动
+     */
     override fun onDraw(canvas: Canvas) {
+        // 3
+        canvas.translate(offsetX, offsetY)
+
         /**
+         *  2
          *  当前缩放的大小 = 初始值 + 差值 * 百分比
          *  差值 = 最大值 - 最小值
          */
         val scale = smallScale + (bigScale - smallScale) * scaleFraction
         canvas.scale(scale, scale, width / 2f, height / 2f)
 
-        canvas.drawBitmap(bitmap, offsetX, offsetY, paint)
+        // 1
+        canvas.drawBitmap(bitmap, originOffsetX, originOffsetY, paint)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -93,6 +126,10 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
         return gestureDetector.onTouchEvent(event)
     }
 
+    /**
+     *  onDown 一定要返回 true
+     *  其他方法返回值随意，是给系统看的
+     */
     override fun onDown(e: MotionEvent?): Boolean {
         // true 表示 gestureDetector 消费事件
         return true
@@ -113,7 +150,8 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
     }
 
     /**
-     * 手指移动，相当于 Move 事件
+     * onScroll 不是 view 移动
+     * 是手指移动，相当于手指在 view 上的 Move 事件
      * distanceX, distanceY 是开始位置 - 当前位置的值
      * 和平时用的相反
      *
@@ -125,11 +163,25 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
         distanceX: Float,
         distanceY: Float
     ): Boolean {
+        if (!isSmall) {
+            offsetX -= distanceX
+            /**
+             *  限制 offsetX 的范围，以免滑出屏幕
+             *  -（图片宽度 - 屏幕宽度）/ 2 <= offsetX <= （图片宽度 - 屏幕宽度）/ 2
+             */
+            offsetX = min(offsetX, (bitmap.width * bigScale - width) / 2)
+            offsetX = max(offsetX, -(bitmap.width * bigScale - width) / 2)
+            offsetY -= distanceY
+            // 限制 offsetX 的范围，同 offsetX
+            offsetY = min(offsetY, (bitmap.height * bigScale - height) / 2)
+            offsetY = max(offsetY, -(bitmap.height * bigScale - height) / 2)
+            invalidate()
+        }
         return true
     }
 
     /**
-     * 快滑
+     * 松手后，快滑
      * velocityX, velocityY 单位时间的位移
      * 返回值无所谓
      */
@@ -139,7 +191,26 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
         velocityX: Float,
         velocityY: Float
     ): Boolean {
+        if (!isSmall) {
+            scroller.fling(
+                offsetX.toInt(), offsetY.toInt(), velocityX.toInt(), velocityY.toInt(),
+                (-(bitmap.width * bigScale - width) / 2).toInt(), // 最小x
+                ((bitmap.width * bigScale - width) / 2).toInt(), // 最大x
+                (-(bitmap.height * bigScale - height) / 2).toInt(), // 最小y
+                ((bitmap.height * bigScale - height) / 2).toInt()) // 最大y
+            // postOnAnimation 下一帧时，去执行动画
+            postOnAnimation(this)
+        }
         return true
+    }
+
+    override fun run() {
+        if (scroller.computeScrollOffset()) { // 只要还在滚动，就继续调用动画
+            offsetX = scroller.currX.toFloat()
+            offsetY = scroller.currY.toFloat()
+            invalidate()
+            postOnAnimation(this)
+        }
     }
 
     override fun onLongPress(e: MotionEvent?) {
@@ -149,6 +220,9 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
         return true
     }
 
+    /**
+     *  双击
+     */
     override fun onDoubleTap(e: MotionEvent?): Boolean {
         isSmall = !isSmall // 双击时，改变动画的大小
         if (isSmall) {
