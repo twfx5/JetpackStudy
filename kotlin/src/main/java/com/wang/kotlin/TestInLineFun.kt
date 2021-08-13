e6f999a2d08d14c80302c7739a98493e6964998c
wangzh <wangzh526@126.com>
2021-08-13 21:16:31 +0800

    add inline comment

--- kotlin/src/main/java/com/wang/kotlin/TestInLineFun.kt
+++ kotlin/src/main/java/com/wang/kotlin/TestInLineFun.kt
@@ -13,9 +13,12 @@ fun main() {
     /**
      * 这里调用 inline 函数 log
      * 在编译时，把 log 里面的代码拷贝到了这里
-     * 所以如果 log 里面代码行数很多，或者 log 在多处地方调用了
+     * 但是，所以如果 log 里面代码行数很多，或者 log 在多处地方调用了
      * 那么对编译的速度有影响
+     *
+     * 只是仅仅减少了调用栈，这不是inline关键字的最佳用法
      */
+    log()
 
     /**
      * 调用 test
@@ -28,6 +31,12 @@ fun main() {
     }
 }
 
+/**
+ * 这里编辑器会给出警告，提示我们这里不需要使用 inline 关键字
+ * inline 关键字最佳使用应该是和 参数 是 函数类型的 参数 一起使用
+ * Expected performance impact from inlining is insignificant.
+ * Inlining works best for functions with parameters of functional types
+ */
 inline fun log() {
     Log.e(TAG, "log: ")
     Log.e(TAG, "log: ")
